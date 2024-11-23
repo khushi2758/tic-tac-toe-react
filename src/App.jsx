6338d7eb87a7228dd5a27f8460e21dc17295165c
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2024-11-24 04:03:09 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,30 @@ import { useState } from "react";
 import GameBoard from "./component/GameBoard";
 import Player from "./component/Player";
 import Log from "./component/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+   currentPlayer = 'O';
+  
+}
+return currentPlayer;
+}
 function App() {
-  
-  
-
-  const [gameTurns, setGameTurns] = useState([]);
+   const [gameTurns, setGameTurns] = useState([]);
   //const[activePlayer, setActivePlayer]= useState('X');
-
+  const activePlayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndx, colIndx){
    // setActivePlayer((curActivePlayer)=> curActivePlayer === 'X'? 'O': 'X');
-   let currentPlayer = 'X';
-   if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
-    currentPlayer = 'O';
-  }
 
 
-    setGameTurns((prevTern) => {
-      let currentPlayer = 'X';
-
-      if(prevTern.length > 0 && prevTern[0].player === 'X'){
-        currentPlayer = 'O';
-      }
-      const udatedTurns = [
+    setGameTurns((prevTerns) => {
+  const currentPlayer = deriveActivePlayer(prevTerns);
+      const updatedTurns = [
         { square: {row: rowIndx, col: colIndx}, player: currentPlayer },
-         ...prevTern,
+         ...prevTerns,
         ];
-         return udatedTurns;
+         return updatedTurns;
     });
   }
 
@@ -35,8 +33,8 @@ function App() {
     <main>
   <div id="game-container">
     <ol id="players" className="highlight-player">
-   <Player initialName="player 1" Symbol="X" isActive={activePlayer === 'X'}/>
-   <Player initialName="player 2" Symbol="O"isActive={activePlayer === 'O'}/>
+   <Player initialName="player 1" Symbol="X" isActive={activePlayer =='X'} />
+   <Player initialName="player 2" Symbol="O" isActive={activePlayer =='O'}/>
   
     </ol>
   <GameBoard 
@@ -47,7 +45,7 @@ function App() {
   </div>
   <Log turns={gameTurns}/>
   </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/GameBoard.jsx
+++ src/component/GameBoard.jsx
@@ -33,7 +33,10 @@ export default function GameBoard({onSelectSquare,turns}){//deriving state
                 <ol>
                     {row.map((playerSymbol, colIndex)=>
                     <li key = {colIndex}>
-                         <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                         <button onClick={() => onSelectSquare(rowIndex,colIndex)} 
+                         disabled={playerSymbol !== null}>
+                            {playerSymbol}
+                         </button>
                     </li> 
                    
                     )}
